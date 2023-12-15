2a05fe52db88c56476ce5b657f9bcf10a9085b41
stackswithans <stexor12@gmail.com>
2023-12-15 01:34:41 +0100

    add visitor protocol

--- pyml/ast.py
+++ pyml/ast.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Protocol, Any
 from dataclasses import dataclass
 from enum import Enum
 import pprint
@@ -9,8 +10,35 @@ class LiteralType(Enum):
     String = 1
 
 
+class Visitor(Protocol):
+    def visit_siblings(self, node: Siblings):
+        ...
+
+    def visit_element(self, node: Element):
+        ...
+
+    def visit_attribute(self, node: Attribute):
+        ...
+
+    def visit_literal(self, node: Literal):
+        ...
+
+    def visit_name(self, node: Name):
+        ...
+
+
 @dataclass
 class Node:
+    def visit(self, visitor: Visitor):
+        cls_name = self.__class__.__name__
+
+        visitor_fn = getattr(visitor, f"visit_{cls_name.lower()}", None)
+        if not visitor_fn:
+            raise NotImplementedError(
+                f"Visitor method not found for node type: {cls_name}"
+            )
+        visitor_fn(self)
+
     def __str__(self) -> str:
         return pprint.pformat(self)
 
@@ -21,8 +49,9 @@ class Siblings(Node):
 
 
 @dataclass
-class Expr(Node):
-    pass
+class Expr(Protocol):
+    def eval(self) -> Any:
+        ...
 
 
 @dataclass
@@ -32,14 +61,20 @@ class Attribute(Node):
 
 
 @dataclass
-class Literal(Expr):
+class Literal(Node, Expr):
     lit_type: LiteralType
     value: str | int | float
 
+    def eval(self) -> Any:
+        return self.value
+
 
 @dataclass
-class Name(Expr):
+class Name(Node, Expr):
     ident: str
+
+    def eval(self) -> Any:
+        return self.ident
 
 
 @dataclass
--- pyml/macros.py
+++ pyml/macros.py
@@ -1,7 +1,6 @@
 import expan
 import pprint
 import pyml.parsing as pymlparsing
-from pyml.elements import Compilable
 from io import StringIO
 from typing import cast
 from pyml.ast import Node
@@ -13,7 +12,6 @@ def pysx(arg: str) -> str:
     root: Node = cast(Node, pymlparsing.pysx_parser.parse_string(arg, True)[0])
     pprint.pprint(root)
     buffer = StringIO()
-    Compilable.from_node(root).compile(buffer)
     pprint.pprint(buffer.getvalue())
     buffer.close()
     return "'Hello world'"
